81b7f8f95ed355c963bd1162ba5ca2c85d3c4ce5
mart-r <mart.ratas@gmail.com>
2025-06-12 10:47:52 +0100

    CU-8699dg1e2: Import MetaCAT and relevant addon within package init

--- medcat-v2/medcat/components/addons/meta_cat/__init__.py
+++ medcat-v2/medcat/components/addons/meta_cat/__init__.py
@@ -1,6 +1,10 @@
 from medcat.utils.import_utils import ensure_optional_extras_installed
 import medcat
 
+from .meta_cat import MetaCAT, MetaCATAddon
+
+
+__all__ = ["MetaCAT", "MetaCATAddon"]
 
 # NOTE: the _ is converted to - automatically
 _EXTRA_NAME = "meta-cat"
