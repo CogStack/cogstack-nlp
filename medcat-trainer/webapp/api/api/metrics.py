d143c9b5baa17519d69a27825362d475954783d8
mart-r <mart.ratas@gmail.com>
2025-06-23 10:48:22 +0100

    Update CDB/Vocab load to use the load classmethod again

--- medcat-trainer/webapp/api/api/metrics.py
+++ medcat-trainer/webapp/api/api/metrics.py
@@ -14,7 +14,7 @@ from background_task.models import Task
 from django.contrib.auth.models import User
 from django.db.models import QuerySet
 from medcat.cat import CAT
-from medcat.storage.serialisers import deserialise
+from medcat.cdb import CDB
 from medcat.config.config_meta_cat import ConfigMetaCAT
 from medcat.components.addons.meta_cat.meta_cat import MetaCAT
 from medcat.components.addons.meta_cat.mctokenizers.tokenizers import TokenizerWrapperBase
@@ -50,8 +50,8 @@ def calculate_metrics(project_ids: List[int], report_name: str):
         loaded_model_pack = True
     else:
         # assume the cdb / vocab is set in these projects
-        cdb = deserialise(projects[0].concept_db.cdb_file.path)
-        vocab = deserialise(projects[0].vocab.vocab_file.path)
+        cdb = CDB.load(projects[0].concept_db.cdb_file.path)
+        vocab = Vocab.load(projects[0].vocab.vocab_file.path)
         cat = CAT(cdb, vocab, config=cdb.config)
     project_data = retrieve_project_data(projects)
     metrics = ProjectMetrics(project_data, cat)
