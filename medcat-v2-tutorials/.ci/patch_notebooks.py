0e1afab2c0602350392136da896f9993732bee91
mart-r <mart.ratas@gmail.com>
2025-07-03 17:29:07 +0100

    CU-8699nbgbh: Patch requirements.txt alongside notebooks

--- medcat-v2-tutorials/.ci/patch_notebooks.py
+++ medcat-v2-tutorials/.ci/patch_notebooks.py
@@ -11,19 +11,29 @@ repo_url = "git+https://github.com/CogStack/cogstack-nlp"
 shell_pattern = re.compile(
     r'(!\s*pip\s+install\s+)(\\["\']?)medcat(\[.*?\])(\s*@\s*git\+[^"\'\s]+)?\2'
 )
+req_txt_pattern = re.compile(
+    r'^(medcat(\[.*?\])?)\s*@\s*git\+[^ \n]+'
+)
 
 
-def do_patch(nb_path: pathlib.Path):
+def repl_nb(m):
+    extras = m[3]
+    old_url = m[4]
+    if old_url and "medcat/v" in old_url:
+        print(f"[WARN] {nb_path} refers to alpha/tagged release: {old_url.strip()}")
+    return f'{m[1]}\\"medcat{extras} @ {repo_url}@{branch}#subdirectory=medcat-v2\\"'
+
+
+def repl_req(m):
+    extras = m[2] if m[2] else ""
+    return f"medcat{extras} @ {repo_url}@{branch}#subdirectory=medcat-v2"
+
+
+def do_patch(nb_path: pathlib.Path,
+             regex: re.Pattern = shell_pattern, repl_method=repl_nb):
     nb_text = nb_path.read_text(encoding="utf-8")
 
-    def repl(m):
-        extras = m[3]
-        old_url = m[4]
-        if old_url and "medcat/v" in old_url:
-            print(f"[WARN] {nb_path} refers to alpha/tagged release: {old_url.strip()}")
-        return f'{m[1]}\\"medcat{extras} @ {repo_url}@{branch}#subdirectory=medcat-v2\\"'
-
-    new_text = shell_pattern.sub(repl, nb_text)
+    new_text = regex.sub(repl_method, nb_text)
 
     if nb_text != new_text:
         nb_path.write_text(new_text, encoding="utf-8")
@@ -32,3 +42,6 @@ def do_patch(nb_path: pathlib.Path):
 
 for nb_path in pathlib.Path(".").rglob("notebooks/**/*.ipynb"):
     do_patch(nb_path)
+
+# Patch requirements.txt as well
+do_patch(pathlib.Path("requirements.txt"), regex=req_txt_pattern, repl_method=repl_req)
