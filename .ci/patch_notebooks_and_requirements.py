eca033f5e1e3d06988627045620a360f8dba2280
mart-r <mart.ratas@gmail.com>
2025-07-04 10:12:07 +0100

    CU-8699nbgbh: Update patch method to be more robust / flexible

--- .ci/patch_notebooks_and_requirements.py
+++ .ci/patch_notebooks_and_requirements.py
@@ -1,47 +1,71 @@
 import sys
 import pathlib
 import re
+from functools import partial
 
-branch = sys.argv[1]
+
 repo_url = "git+https://github.com/CogStack/cogstack-nlp"
 
 # Matches either:
 # 1. `! pip install medcat[extras]`
 # 2. `! pip install medcat[extras] @ git+...`
 shell_pattern = re.compile(
-    r'(!\s*pip\s+install\s+)(\\["\']?)medcat(\[.*?\])(\s*@\s*git\+[^"\'\s]+)?\2'
+    r'(!\s*pip\s+install\s+)(\\["\']?)medcat(\[.*?\])'
+    r'(\s*@\s*git\+[^"\'\s]+)?\2'
 )
 req_txt_pattern = re.compile(
-    r'^(medcat(\[.*?\])?)\s*@\s*git\+[^ \n]+'
+    r'^(medcat(\[.*?\])?)\s*@\s*git\+\S+', flags=re.MULTILINE
 )
 
 
-def repl_nb(m):
+def repl_nb(m, file_path: pathlib.Path, branch: str):
     extras = m[3]
     old_url = m[4]
     if old_url and "medcat/v" in old_url:
-        print(f"[WARN] {nb_path} refers to alpha/tagged release: {old_url.strip()}")
-    return f'{m[1]}\\"medcat{extras} @ {repo_url}@{branch}#subdirectory=medcat-v2\\"'
+        print(f"[WARN] {file_path} refers to alpha/tagged release: "
+              f"{old_url.strip()}")
+    return (f'{m[1]}\\"medcat{extras} @ {repo_url}@{branch}'
+            '#subdirectory=medcat-v2\\"')
 
 
-def repl_req(m):
+def repl_req(m, file_path: pathlib.Path, branch: str):
     extras = m[2] if m[2] else ""
-    return f"medcat{extras} @ {repo_url}@{branch}#subdirectory=medcat-v2"
+    return (f"medcat{extras} @ {repo_url}@{branch}"
+            "#subdirectory=medcat-v2")
 
 
-def do_patch(nb_path: pathlib.Path,
+def do_patch(nb_path: pathlib.Path, branch: str,
              regex: re.Pattern = shell_pattern, repl_method=repl_nb):
     nb_text = nb_path.read_text(encoding="utf-8")
 
-    new_text = regex.sub(repl_method, nb_text)
+    repl = partial(repl_method, file_path=nb_path, branch=branch)
+    new_text = regex.sub(repl, nb_text)
 
     if nb_text != new_text:
         nb_path.write_text(new_text, encoding="utf-8")
         print(f"[PATCHED] {nb_path}")
 
 
-for nb_path in pathlib.Path(".").rglob("notebooks/**/*.ipynb"):
-    do_patch(nb_path)
+def main(path: str, branch: str):
+    for nb_path in pathlib.Path(path).rglob("**/*.ipynb"):
+        do_patch(nb_path, branch)
 
-# Patch requirements.txt as well
-do_patch(pathlib.Path("requirements.txt"), regex=req_txt_pattern, repl_method=repl_req)
+    # Patch requirements.txt as well
+    do_patch(pathlib.Path(path, "requirements.txt"), branch,
+             regex=req_txt_pattern, repl_method=repl_req)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python patch_notebooks_and_requirements.py "
+              "<path> <branch>")
+        sys.exit(1)
+
+    path = sys.argv[1]
+    branch = sys.argv[2]
+
+    if not pathlib.Path(path).exists():
+        print(f"Path {path} does not exist.")
+        sys.exit(1)
+
+    main(path, branch)
