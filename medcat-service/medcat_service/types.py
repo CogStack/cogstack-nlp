c922cb3525ad0fca7248bd384cdc1935ecc28529
alhendrickson <159636032+alhendrickson@users.noreply.github.com.>
2025-07-17 14:58:48 +0000

    refactor(medcat-service): Add pydantic. Add types to Health and Info APIs

--- medcat-service/medcat_service/types.py
+++ medcat-service/medcat_service/types.py
@@ -5,12 +5,14 @@ from pydantic import BaseModel
 
 BaseModel.model_config['protected_namespaces'] = ()
 
+
 class HealthCheckResponse(BaseModel):
     """
     Using Eclipse MicroProfile health response schema
     """
     name: str
     status: Literal["UP", "DOWN"]
+
 
 class HealthCheckResponseContainer(BaseModel):
     status: Literal["UP", "DOWN"]
@@ -21,6 +23,7 @@ class ModelCardInfo(BaseModel):
     ontologies: Union[str, List[str], None]
     meta_cat_model_names: list[str]
     model_last_modified_on: Optional[datetime]
+
 
 class ServiceInfo(BaseModel):
     service_app_name: str
