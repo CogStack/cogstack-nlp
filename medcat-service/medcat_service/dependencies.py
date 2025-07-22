31c3bdfa90aeae75800877bc7cef1777769a30ee
alhendrickson <159636032+alhendrickson@users.noreply.github.com.>
2025-07-22 11:30:58 +0000

    CU-8699wvj3d refactor(medcat-service): Add FastAPI framework and Health API

--- medcat-service/medcat_service/dependencies.py
+++ medcat-service/medcat_service/dependencies.py
@@ -1,10 +1,12 @@
 from typing import Annotated
 
 from fastapi import Depends
+
 from medcat_service.nlp_processor.medcat_processor import MedCatProcessor
 
 # Singleton instance
 _medcat_processor = MedCatProcessor()
+
 
 # Dependency function
 def get_medcat_processor() -> MedCatProcessor:
