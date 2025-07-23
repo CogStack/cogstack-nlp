acc156a7ef1848d8a9fa138be6e240ce9e0da0b4
alhendrickson <159636032+alhendrickson@users.noreply.github.com.>
2025-07-23 11:29:35 +0000

    refactor(medcat-service): Add process examples

--- medcat-service/medcat_service/routers/process.py
+++ medcat-service/medcat_service/routers/process.py
@@ -13,29 +13,37 @@ router = APIRouter(tags=["Process"])
 
 @router.post("/api/process")
 def process(
-        payload: Annotated[
-            ProcessAPIInput,
-            Body(
-                examples=[
-                    {
+    payload: Annotated[
+        ProcessAPIInput,
+        Body(
+            openapi_examples={
+                "normal": {
+                    "summary": "A normal example",
+                    "description": "Normal use puts a free text note in the text field",
+                    "value": {
                         "content": {
                             "text": "Patient had been diagnosed with acute Kidney Failure the week before",
                         }
                     },
-                    {
+                },
+                "complete": {
+                    "summary": "A complete example",
+                    "description": "Complete use adds a footer and meta annotations filter.",
+                    "value": {
                         "content": {"text": "Patient had been diagnosed with acute Kidney Failure the week before", "footer": "string"},
                         "meta_anns_filters": [("Presence", ["True"]), ("Subject", ["Patient", "Family"])],
                     },
-                ]
-            ),
-        ],
-        medcat_processor: MedCatProcessorDep) -> ProcessAPIResponse:
+                },
+            }
+        ),
+    ],
+    medcat_processor: MedCatProcessorDep,
+) -> ProcessAPIResponse:
     """
     Returns the annotations extracted from a provided single document
     """
     try:
-        process_result = medcat_processor.process_content(
-            payload.content.model_dump(), meta_anns_filters=payload.meta_anns_filters)
+        process_result = medcat_processor.process_content(payload.content.model_dump(), meta_anns_filters=payload.meta_anns_filters)
         app_info = medcat_processor.get_app_info()
         return ProcessAPIResponse(result=process_result, medcat_info=app_info)
     except Exception as e:
