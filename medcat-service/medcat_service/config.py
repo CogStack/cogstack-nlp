882252b3465b9d28c1c619b3b35729aaa054f9a8
alhendrickson <159636032+alhendrickson@users.noreply.github.com.>
2025-07-29 11:55:29 +0000

    feat(medcat-service: Add root_path support. Add settings as a dependency

--- medcat-service/medcat_service/config.py
+++ medcat-service/medcat_service/config.py
@@ -3,4 +3,6 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    app_root_path: str = Field(default="/", description="The Root Path for the FastAPI App", examples=["/medcat-service"])
+    app_root_path: str = Field(
+        default="/", description="The Root Path for the FastAPI App", examples=["/medcat-service"]
+    )
