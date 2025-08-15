ab39cd2649a9cd5198d308890bc85dde9641e9c4
alhendrickson <159636032+alhendrickson@users.noreply.github.com.>
2025-08-15 16:00:50 +0000

    test(medcat-service: 869a17yed Create integration test for DeID with pretrained example model

--- medcat-service/start_service_debug.sh
+++ medcat-service/start_service_debug.sh
@@ -2,7 +2,7 @@
 echo "Starting MedCAT Service"
 
 if [ -z "${APP_MODEL_CDB_PATH}" ] && [ -z "${APP_MODEL_VOCAB_PATH}" ] && [ -z "${APP_MEDCAT_MODEL_PACK}" ]; then
-  # export APP_MEDCAT_MODEL_PACK="models/examples/example-medcat-v2-model-pack.zip"
+  export APP_MEDCAT_MODEL_PACK="models/examples/example-medcat-v2-model-pack.zip"
   echo "Using default model pack in  $APP_MEDCAT_MODEL_PACK"
 fi
 
