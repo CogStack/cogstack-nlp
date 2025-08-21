efec41737d192e45e9bcde124608cda773f2df5d
alhendrickson <159636032+alhendrickson@users.noreply.github.com.>
2025-08-21 13:48:47 +0000

    feat(medcat-service): Use Pydantic Settings in MedCATProcessor for DeID

--- medcat-service/medcat_service/dependencies.py
+++ medcat-service/medcat_service/dependencies.py
@@ -1,3 +1,4 @@
+import logging
 from functools import lru_cache
 from typing import Annotated
 
@@ -6,17 +7,19 @@ from fastapi import Depends
 from medcat_service.config import Settings
 from medcat_service.nlp_processor.medcat_processor import MedCatProcessor
 
+log = logging.getLogger(__name__)
+
 
 @lru_cache
 def get_settings() -> Settings:
     settings = Settings()
-    print("Using settings " + str(settings))
+    log.debug("Using settings " + str(settings))
     return settings
 
 
 @lru_cache
 def get_medcat_processor(settings: Annotated[Settings, Depends(get_settings)]) -> MedCatProcessor:
-    print("Using settings for medcat processor " + str(settings))
+    log.debug("Creating new Medcat Processsor using settings " + str(settings))
     return MedCatProcessor(settings)
 
 
