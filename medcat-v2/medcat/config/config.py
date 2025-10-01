a03a62f76c4bf8c3facd9b9cd5c6a4f637610c6c
Adam Sutton <sutton.adam@hotmail.co.uk>
2025-10-01 12:49:34 +0100

    changes to config documentation and filter before disambig

--- medcat-v2/medcat/config/config.py
+++ medcat-v2/medcat/config/config.py
@@ -377,47 +377,48 @@ class Linking(ComponentConfig):
         extra = 'allow'
 
 class EmbeddingLinking(Linking):
-    """Changing compoenent name"""
+    """The config exclusively used for the embedding linker"""
     comp_name: str = "embedding_linker"
-    """All concepts below this will always be disambiguated"""
-    filter_before_disamb: bool = True
+    """Changing compoenent name"""
+    filter_before_disamb: bool = False
+    """Filtering CUIs before disambiguation"""
+    train: bool = False
     """The embedding linker never needs to be trained in its 
     current implementation."""
-    train: bool = False
+    long_similarity_threshold: float = 0.0
     """Used in the inference step to choose the best CUI given the
     link candidates. Testing shows a threshold of 0.7 increases precision
     with minimal impact on recall. Default is 0.0 which assumes
     all entities detected by the NER step are true."""
-    long_similarity_threshold: float = 0.0
+    short_similarity_threshold: float = 0.0
     """Used for generating cui candidates. If a threshold of 0.0
     is selected then only the highest scoring name will provide cuis
     to be link candidates. Use a threshold of 0.95 or higher, as this is
     essentailly string matching and account for spelling errors. Lower 
     thresholds will provide too many candidates and slow down the inference."""
-    short_similarity_threshold: float = 0.0
+    embedding_model_name: str = "sentence-transformers/all-MiniLM-L6-v2"
     """Name of the embedding model. It must be downloadable from 
     huggingface linked from an appropriate file directory"""
-    embedding_model_name: str = "sentence-transformers/all-MiniLM-L6-v2"
+    max_token_length: int = 64
     """Max number of tokens to be embedded from a name.
     If the max token length is changed then the linker will need to be created
     with a new config.
     """
-    max_token_length: int = 64
+    embedding_batch_size: int = 4096
     """How many pieces names can be embedded at once, useful when 
     embedding name2info names, cui2info names"""
-    embedding_batch_size: int = 4096
-    """How many entities to be linked at once"""
     linking_batch_size: int = 512
+    """How many entities to be linked at once"""
+    gpu_device: Optional[Any] = None
     """Choose a device for the linking model to be stored. If None
     then an appropriate GPU device that is available will be chosen"""
-    gpu_device: Optional[Any] = None
-    """Choose the window size to get context vectors."""
     context_window_size: int = 14
+    """Choose the window size to get context vectors."""
+    use_ner_link_candidates: bool = True
     """Link candidates are provided by some NER steps. This will flag if 
     you want to trust them or not."""
-    use_ner_link_candidates: bool = True
-    """Do we have a similarity threshold we care about?"""
     use_similarity_threshold: bool = True
+    """Do we have a similarity threshold we care about?"""
 
 class Preprocessing(SerialisableBaseModel):
     """The preprocessing part of the config"""
