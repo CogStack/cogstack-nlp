1a95506e7f2711b59df81e51ab1da9991889a799
jocelyneholdbrook <jocelyne@cogstack.org>
2025-10-02 06:07:28 +0100

    CU-869ajt55f: Revert unintended changes

--- medcat-trainer/webapp/frontend/vite.config.ts
+++ medcat-trainer/webapp/frontend/vite.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
         rewrite: (path) => path.replace(/\/api\/concepts/, '/')
       },
       '^/api/*': {
-        target: 'http://127.0.0.1:8000',
+        target: 'http://127.0.0.1:8001'
       }
     }
   },
