6702652d5d14c6ac66a4cc73464c9d0bbb56c4af
mart-r <mart.ratas@gmail.com>
2025-10-22 13:55:14 +0100

    CU-869aa22g2: Fail upon too large a size when scrolling

--- cogstack-es/cogstack.py
+++ cogstack-es/cogstack.py
@@ -676,6 +676,9 @@ class CogStack:
                 self.elastic.clear_scroll(scroll_id=search_scroll_id)
                 print("Request cancelled and current "
                       "search_scroll_id deleted...")
+            elif isinstance(err, ValueError) and err.args == (
+                    'Size must not be greater than 10000',):
+                raise err
             else:
                 if pr_bar is not None:
                     pr_bar.bar_format = "%s{l_bar}%s{bar}%s{r_bar}" % (
