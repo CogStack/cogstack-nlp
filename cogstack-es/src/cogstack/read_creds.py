90cbc61eb4d447ba951340ce64b7f4b5687d09a4
mart-r <mart.ratas@gmail.com>
2025-10-24 16:08:11 +0100

    CU-869aa22g2: Update credentials to use ID and API key and/or encoded values

--- cogstack-es/src/cogstack/read_creds.py
+++ cogstack-es/src/cogstack/read_creds.py
@@ -4,7 +4,23 @@ import os
 CS_HOSTS_ENV = "COGSTACK_HOSTS"
 CS_UN_ENV = "COGSTACK_USERNAME"
 CS_PW_ENV = "COGSTACK_PASSWORD"
-CS_API_KEY_ENV = "COGSTACK_API_KEY"
+CS_API_KEY_ID_ENV = "COGSTACK_API_KEY_ID"
+CS_API_KEY_KEY_ENV = "COGSTACK_API_KEY"
+CS_API_KEY_ENCODED_ENV = "COGSTACK_API_KEY_ENCODED"
+
+
+def _read_api_key() -> dict | None:
+    key_id = os.getenv(CS_API_KEY_ID_ENV)
+    api_key = os.getenv(CS_API_KEY_KEY_ENV)
+    api_key_encoded = os.getenv(CS_API_KEY_ENCODED_ENV)
+    out_dict = {}
+    if key_id:
+        out_dict["id"] = key_id
+    if api_key:
+        out_dict["api_key"] = api_key
+    if api_key_encoded:
+        out_dict["encoded"] = api_key_encoded
+    return out_dict or None
 
 
 def read_from_env() -> tuple[list[str],
@@ -20,12 +36,7 @@ def read_from_env() -> tuple[list[str],
                 the username-password pair.
     """
     hosts = os.getenv(CS_HOSTS_ENV, "").split(",")
-    api_key = (
-        # TODO: is this correct?
-        {"encoded": api_key}
-        if (api_key := os.environ.get(CS_API_KEY_ENV))
-        else None
-    )
+    api_key = _read_api_key()
     username = os.getenv(CS_UN_ENV)
     password = os.getenv(CS_PW_ENV)
     return hosts, api_key, (username, password)
