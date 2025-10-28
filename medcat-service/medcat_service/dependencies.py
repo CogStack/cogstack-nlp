a25265c9bac6507ac9c90481b8f57bface032ca9
alhendrickson <159636032+alhendrickson@users.noreply.github.com.>
2025-10-28 18:03:25 +0000

    fix(medcat-service): Add thread safety to medcat processor dependency

--- medcat-service/medcat_service/dependencies.py
+++ medcat-service/medcat_service/dependencies.py
@@ -1,6 +1,7 @@
 import logging
+import threading
 from functools import lru_cache
-from typing import Annotated
+from typing import Annotated, Optional
 
 from fastapi import Depends
 
@@ -13,14 +14,27 @@ log = logging.getLogger(__name__)
 @lru_cache
 def get_settings() -> Settings:
     settings = Settings()
-    log.debug("Using settings: %s", settings)
+    log.info(f"Starting service using settings: '{settings}'")
     return settings
+
+
+_def_medcat_processor: Optional[MedCatProcessor] = None
+_def_medcat_processor_lock = threading.Lock()
+
+
+def get_medcat_processor_singleton(settings: Settings) -> MedCatProcessor:
+    with _def_medcat_processor_lock:
+        global _def_medcat_processor
+        if _def_medcat_processor is None:
+            log.info("Creating new Medcat Processsor using settings: %s", settings)
+            _def_medcat_processor = MedCatProcessor(settings)
+        return _def_medcat_processor
 
 
 @lru_cache
 def get_medcat_processor(settings: Annotated[Settings, Depends(get_settings)]) -> MedCatProcessor:
-    log.debug("Creating new Medcat Processsor using settings: %s", settings)
-    return MedCatProcessor(settings)
+    log.debug("Creating new medcat processor from cache miss")
+    return get_medcat_processor_singleton(settings)
 
 
 MedCatProcessorDep = Annotated[MedCatProcessor, Depends(get_medcat_processor)]
