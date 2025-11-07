f138663680bdb9116765f6bef7bd530e348597cb
mart-r <mart.ratas@gmail.com>
2025-11-07 11:10:14 +0000

    Add script to get overall results (startup, warm, cold)

---
+++ medcat-v2/paper/scripts/get_load_speed_all.py
@@ -0,0 +1,97 @@
+from pydantic import BaseModel, ConfigDict
+# import runpy
+import subprocess
+import os
+import sys
+import argparse
+from pprint import pprint
+from enum import Enum, auto
+
+import get_load_speed
+
+
+class RunConfig(BaseModel):
+    repeats: int = 20
+    # how many times to perform for warmup
+    warmup_count: int = 1
+
+
+class RunResults(BaseModel):
+    all_times: list[float]
+    mean: float
+    min: float
+    max: float
+
+    model_config = ConfigDict(frozen=True)
+
+    @classmethod
+    def from_times(cls, times: list[float]) -> "RunResults":
+        return cls(
+            all_times=times,
+            mean=sum(times) / len(times),
+            min=min(times),
+            max=max(times),
+        )
+
+
+class OverallResults(BaseModel):
+    startup: RunResults
+    cold: RunResults
+    warm: RunResults
+
+
+class RunType(Enum):
+    STARTUP = auto()
+    COLD = auto()
+    WARM = auto()
+
+
+def _single_experiment(model_path: str,
+                       cnf: RunConfig,
+                       run_type: RunType,
+                       ) -> RunResults:
+    target_script = os.path.join(
+        os.path.dirname(__file__), get_load_speed.__name__ + ".py")
+    sys_argv = [sys.executable, target_script, model_path,]
+    if run_type is RunType.STARTUP:
+        sys_argv.extend(["-w", "0", "-s"])
+    elif run_type is RunType.COLD:
+        sys_argv.extend(["-w", "0"])
+    elif run_type is RunType.WARM:
+        sys_argv.extend(["-w", str(cnf.warmup_count)])
+    all_took: list[float] = []
+    for _ in range(cnf.repeats):
+        run_out = subprocess.run(sys_argv, capture_output=True)
+        all_took.append(float(run_out.stdout))
+    return RunResults.from_times(all_took)
+
+
+def do_experiment(
+        model_path: str,
+        cnf: RunConfig = RunConfig(),
+        ) -> OverallResults:
+    return OverallResults(
+        startup=_single_experiment(
+            model_path, cnf, RunType.STARTUP),
+        cold=_single_experiment(
+            model_path, cnf, RunType.COLD),
+        warm=_single_experiment(
+            model_path, cnf, RunType.WARM)
+    )
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        "get_load_speed_all"
+    )
+    parser.add_argument("model_pack_path",
+                        help="Model pack path",
+                        type=str)
+    args = parser.parse_args()
+    results = do_experiment(args.model_pack_path, RunConfig(repeats=3))
+    print("Overall:")
+    pprint(results.model_dump())
+
+
+if __name__ == "__main__":
+    main()
