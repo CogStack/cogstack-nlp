7f2ff90afe79654dfecc5df3b454d2cac78008ed
mart-r <mart.ratas@gmail.com>
2025-11-07 16:54:28 +0000

    Add a few overarching scripts to run all the speed scripts at once

---
+++ medcat-v2/paper/scripts/speed/run_all_speed_scripts_for_version.sh
@@ -0,0 +1,12 @@
+ver=$1
+echo "Running for version: $ver"
+python --version
+python -m pip show medcat | grep "Version"
+
+
+for fn in `ls scripts/speed/*_v1.sh`;
+do
+    echo "Running script:"
+    echo $fn
+    # bash $fn
+done
