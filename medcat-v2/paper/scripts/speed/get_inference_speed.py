58ed8933b0a1bff6c5d12ce35db7f48356200547
mart-r <mart.ratas@gmail.com>
2025-11-13 11:41:49 +0000

    Fix typo

--- medcat-v2/paper/scripts/speed/get_inference_speed.py
+++ medcat-v2/paper/scripts/speed/get_inference_speed.py
@@ -40,7 +40,7 @@ def main():
         setup=["from medcat.cat import CAT",
                "import pandas as pd",
                f"cat = CAT.load_model_pack('{args.model_pack_path}')",
-               "cat.cdb.has_subname('abc')" if mct_ver.startwith("2") else "",
+               "cat.cdb.has_subname('abc')" if mct_ver.startswith("2") else "",
                f"df = pd.read_csv('{args.csv_path}')"],
         worker=["for text in df.text:",
                 "    cat.get_entities(text)"],
