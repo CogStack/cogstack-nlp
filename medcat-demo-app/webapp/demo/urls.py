0fc11b58a5c4a2edb6121a04069b06e9578f9259
mart-r <mart.ratas@gmail.com>
2025-11-20 16:38:51 +0000

    CU-869b855n0: Use questionnaire endpoints

--- medcat-demo-app/webapp/demo/urls.py
+++ medcat-demo-app/webapp/demo/urls.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from django.urls import path
 from .views import *
+from .questionnaire import (
+    get_client_identifier, get_questionnaire, submit_questionnaire, check_api_key)
 
 urlpatterns = [
     path('', show_annotations, name='train_annotations'),
     path('auth-callback', validate_umls_user, name='validate-umls-user'),
     path('auth-callback-api', validate_umls_api_key, name='validate-umls-api-key'),
-    path('download-model', download_model, name="download-model")
+    path('download-model', download_model, name="download-model"),
+    # questionnaire
+    path('umls-license-questionnair/qet-client-id', get_client_identifier,
+         name="get-client-identifier"),
+    path('umls-license-questionnair/qet-questionnaire', get_questionnaire,
+         name="qet-questionnaire"),
+    path('umls-license-questionnair/submit-questionnaire', submit_questionnaire,
+         name="submit-questionnaire"),
+    path('umls-license-questionnair/check-api-key', check_api_key,
+         name="check-api-key"),
 ]
