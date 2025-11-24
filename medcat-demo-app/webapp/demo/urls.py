ae3b3e71388647447b9bd77c0db45307357bd5a1
mart-r <mart.ratas@gmail.com>
2025-11-24 09:25:15 +0000

    CU-869b855n0: Remove questionnaire endpoints

--- medcat-demo-app/webapp/demo/urls.py
+++ medcat-demo-app/webapp/demo/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.urls import path
 from .views import *
-from .questionnaire import (
-    get_client_identifier, get_questionnaire, submit_questionnaire, check_api_key)
 
 urlpatterns = [
     path('', show_annotations, name='train_annotations'),
@@ -10,14 +8,6 @@ urlpatterns = [
     path('auth-callback-api', validate_umls_api_key, name='validate-umls-api-key'),
     path('download-model', download_model, name="download-model"),
     # questionnaire
-    path('umls-license-questionnaire/get-client-id', get_client_identifier,
-         name="get-client-identifier"),
-    path('umls-license-questionnaire/', get_questionnaire,
-         name="get-questionnaire"),
-    path('umls-license-questionnaire/submit-questionnaire', submit_questionnaire,
-         name="submit-questionnaire"),
-    path('umls-license-questionnaire/check-api-key', check_api_key,
-         name="check-api-key"),
     path('callback-after-questionnaire/', model_after_api_key,
          name="model_after_api_key"),
 ]
