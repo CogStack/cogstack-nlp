02e49c264e5ec2ee97f93b4a090468617b31f0d8
mart-r <mart.ratas@gmail.com>
2025-11-24 11:33:53 +0000

    CU-869b855n0: Add permission to manually add API keys

--- medcat-demo-app/webapp/demo/admin.py
+++ medcat-demo-app/webapp/demo/admin.py
@@ -29,9 +29,6 @@ class APIKeyAdmin(admin.ModelAdmin):
     is_expired.boolean = True
     is_expired.short_description = 'Expired'
 
-    def has_add_permission(self, request):
-        return False
-
 
 # Register your models here.
 admin.site.register(UploadedText, UploadedTextAdmin)
