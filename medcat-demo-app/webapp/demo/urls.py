6393910a1f6a5a5402fbe05a8b0d91317dff83a4
mart-r <mart.ratas@gmail.com>
2025-11-24 12:26:31 +0000

    CU-869b855n0: Update manual API callback URL

--- medcat-demo-app/webapp/demo/urls.py
+++ medcat-demo-app/webapp/demo/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('auth-callback-api', validate_umls_api_key, name='validate-umls-api-key'),
     path('download-model', download_model, name="download-model"),
     # questionnaire
-    path('callback-after-questionnaire/', model_after_api_key,
+    path('manual-api-callback/', model_after_api_key,
          name="model_after_api_key"),
 ]
