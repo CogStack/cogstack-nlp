1f92f83f440b7be9b36faf163c2f3a8174ee5913
mart-r <mart.ratas@gmail.com>
2025-11-27 16:59:49 +0000

    Remove some echo /debug output

--- medcat-v2/paper/scripts/variance/get_variance_with_linker_and_tokenizer_all.sh
+++ medcat-v2/paper/scripts/variance/get_variance_with_linker_and_tokenizer_all.sh
@@ -2,42 +2,42 @@ SCRIPT="scripts/variance/get_variance_with_linker_and_tokenizer.py"
 MODEL_PATH="../.temp/CONVERT_2023_model_no_mc_234dda1597f635e3.zip"
 MODEL_PATH="../tests/resources/mct2_model_pack.zip"
 
-echo "==COMETA=="
+#"==COMETA=="
 DATASET="data/supervised/cometa/mct_export.json"
 EXTRA="--one-line"
 
-echo "NORMAL"
+#"NORMAL"
 python $SCRIPT old spacy $MODEL_PATH $DATASET $EXTRA
-echo "With faster linker"
+#"With faster linker"
 python $SCRIPT new spacy $MODEL_PATH $DATASET $EXTRA
-echo "With regex tokenizer"
+#"With regex tokenizer"
 python $SCRIPT old regex $MODEL_PATH $DATASET $EXTRA
-echo "With regex tokenizer AND faster linker"
+# "With regex tokenizer AND faster linker"
 python $SCRIPT new regex $MODEL_PATH $DATASET $EXTRA
 
 # with embedding linker
 
-echo "With spacy tokenizer + embed lnker"
+# "With spacy tokenizer + embed lnker"
 python $SCRIPT embed spacy $MODEL_PATH $DATASET $EXTRA
-echo "With regex tokenizer + embed linker"
+# "With regex tokenizer + embed linker"
 python $SCRIPT embed regex $MODEL_PATH $DATASET $EXTRA
 
 # other dataset
-echo "==Linking Challenge=="
+# "==Linking Challenge=="
 DATASET="data/supervised/linking_challenge/mct_export.json"
 
-echo "NORMAL"
+# "NORMAL"
 python $SCRIPT old spacy $MODEL_PATH $DATASET $EXTRA
-echo "With faster linker"
+# "With faster linker"
 python $SCRIPT new spacy $MODEL_PATH $DATASET $EXTRA
-echo "With regex tokenizer"
+# "With regex tokenizer"
 python $SCRIPT old regex $MODEL_PATH $DATASET $EXTRA
-echo "With regex tokenizer AND faster linker"
+# "With regex tokenizer AND faster linker"
 python $SCRIPT new regex $MODEL_PATH $DATASET $EXTRA
 
 # with embedding linker
 
-echo "With spacy tokenizer + embed lnker"
+# "With spacy tokenizer + embed lnker"
 python $SCRIPT embed spacy $MODEL_PATH $DATASET $EXTRA
-echo "With regex tokenizer + embed linker"
+# "With regex tokenizer + embed linker"
 python $SCRIPT embed regex $MODEL_PATH $DATASET $EXTRA
