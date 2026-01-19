533ddd3a1f1cc2183cc24ed402b5458ca9e04b9e
mart-r <mart.ratas@gmail.com>
2026-01-19 09:23:30 +0000

    CU-869bu4tum: Add a few more tests for wrapper (i.e getting attributes, properties, methods)

--- medcat-den/tests/test_wrappers.py
+++ medcat-den/tests/test_wrappers.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from medcat.cat import CAT
 
 from medcat_den import wrappers
@@ -6,8 +7,7 @@ from .test_file_system_den import def_model_pack
 
 
 def wrap(cat: CAT) -> wrappers.CATWrapper:
-    # creates a new pipe!
-    return wrappers.CATWrapper(cat.cdb, cat.vocab, cat.config)
+    return wrappers.CATWrapper(cat)
 
 
 def test_wrapper_saves_as_CAT(tmpdir, def_model_pack):
@@ -16,3 +16,19 @@ def test_wrapper_saves_as_CAT(tmpdir, def_model_pack):
     loaded = CAT.load_model_pack(mpp)
     assert isinstance(loaded, CAT)
     assert not isinstance(loaded, wrappers.CATWrapper)
+
+
+def test_wrapper_gets_attributes(def_model_pack: CAT):
+    cat = wrap(def_model_pack)
+    assert cat.cdb is def_model_pack.cdb
+
+
+def test_wrapper_gets_properties(def_model_pack: CAT):
+    cat = wrap(def_model_pack)
+    assert cat.pipe is def_model_pack.pipe
+
+
+def test_wrapper_gets_methods(def_model_pack: CAT):
+    cat = wrap(def_model_pack)
+    text = "Kidney disease causes autism and fever in diabetes patients"
+    assert cat.get_entities(text) == def_model_pack.get_entities(text)
