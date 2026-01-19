23c12fb4f402808cd552a0057311b42f02c73cd6
mart-r <mart.ratas@gmail.com>
2026-01-19 13:45:09 +0000

    CU-869bu4tum: Add a few more comprehensive tests

--- medcat-den/tests/test_wrappers.py
+++ medcat-den/tests/test_wrappers.py
@@ -1,19 +1,36 @@
-from unicodedata import category
+import os
+
 from medcat.cat import CAT
+from medcat.storage.schema import load_schema
 
 from medcat_den import wrappers
 
-from .test_file_system_den import def_model_pack
+from .test_file_system_den import def_model_pack, _load_model, MODEL_PATH
+
+import pytest
 
 
 def wrap(cat: CAT) -> wrappers.CATWrapper:
-    return wrappers.CATWrapper(cat)
+    wrapped = wrappers.CATWrapper(cat)
+    wrapped._model_info = "ABC"
+    return wrapped
 
 
-def test_wrapper_saves_as_CAT(tmpdir, def_model_pack):
-    cat = wrap(def_model_pack)
-    mpp = cat.save_model_pack(tmpdir, force_save_local=True)
-    loaded = CAT.load_model_pack(mpp)
+@pytest.fixture(scope='module')
+def saved_model_pack_path(tmpdir_factory) -> str:
+    tmpdir = tmpdir_factory.mktemp('model_path')
+    cat = wrap(_load_model(MODEL_PATH))
+    assert isinstance(cat, wrappers.CATWrapper)
+    return cat.save_model_pack(tmpdir, force_save_local=True)
+
+
+def test_wrapper_schema_CAT(saved_model_pack_path):
+    schema = os.path.join(saved_model_pack_path.removesuffix(".zip"), '.schema.json')
+    assert load_schema(schema)[0] == 'medcat.cat.CAT'
+
+
+def test_wrapper_saves_as_CAT(saved_model_pack_path):
+    loaded = CAT.load_model_pack(saved_model_pack_path)
     assert isinstance(loaded, CAT)
     assert not isinstance(loaded, wrappers.CATWrapper)
 
