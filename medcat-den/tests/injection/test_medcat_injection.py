386f0fe0947adbe1ac800cb154b57bddbfcdc50b
mart-r <mart.ratas@gmail.com>
2026-01-19 10:46:37 +0000

    CU-869bu4tum: Fix injection tests

--- medcat-den/tests/injection/test_medcat_injection.py
+++ medcat-den/tests/injection/test_medcat_injection.py
@@ -59,7 +59,7 @@ def den_with_model(tmp_path: str, cat: CAT) -> Den:
 def den_with_nonempty_model(den_with_model: Den):
     model = den_with_model.fetch_model(
         den_with_model.list_available_base_models()[0])
-    model.vocab = Vocab()
+    model._delegate.vocab = Vocab()
     model.vocab.add_word("acute", 4, np.array([1, 0, 0]))
     model.vocab.add_word("chronic", 3, np.array([-1, 0, 0]))
     model.vocab.add_word("does", 10, np.array([0, 1, 0]))
