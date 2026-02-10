10075a945faa54f2994ca3e76dde0de968e9d408
mart-r <mart.ratas@gmail.com>
2026-02-10 14:37:06 +0000

    CU-869c36ruk: Fix typo for lazy registration method name

--- medcat-plugins/embed-linker/src/medcat_embedding_linker/registration.py
+++ medcat-plugins/embed-linker/src/medcat_embedding_linker/registration.py
@@ -2,13 +2,13 @@
 import logging
 
 from medcat.components.types import CoreComponentType
-from medcat.components.types import lazy_register_core_componet
+from medcat.components.types import lazy_register_core_component
 
 
 logger = logging.getLogger(__name__)
 
 
 def do_registration():
-    lazy_register_core_componet(
+    lazy_register_core_component(
         CoreComponentType.ner, "embedding_linker",
         "medcat_embedding_linker.embedding_linker", "Linker.create_new_component")
