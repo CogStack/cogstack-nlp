0c2107c4caff83fd1606cb7c66edcf3247d9ff9d
mart-r <mart.ratas@gmail.com>
2026-02-10 14:18:29 +0000

    CU-869c36ruk: Add component registration for embedding linker plugin

---
+++ medcat-plugins/embed-linker/src/medcat_embedding_linker/registration.py
@@ -0,0 +1,14 @@
+
+import logging
+
+from medcat.components.types import CoreComponentType
+from medcat.components.types import lazy_register_core_componet
+
+
+logger = logging.getLogger(__name__)
+
+
+def do_registration():
+    lazy_register_core_componet(
+        CoreComponentType.ner, "embedding_linker",
+        "medcat_embedding_linker.embedding_linker", "Linker.create_new_component")
