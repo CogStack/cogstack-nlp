f2ac5303884187985aa7f82eecd0d2a6e298650c
Mart Ratas <mart.ratas@gmail.com>
2026-07-16 15:55:12 +0100

    Update contracting to fix token spying

--- medcat-v2/medcat/components/contracting.py
+++ medcat-v2/medcat/components/contracting.py
@@ -1,6 +1,7 @@
 from enum import Enum, auto
-from typing import Any, Iterator, Callable
+from typing import Any, Iterator, Callable, Type
 from contextlib import contextmanager, ExitStack
+from collections import defaultdict
 from logging import Logger
 
 from medcat.tokenizing.tokens import MutableDocument
@@ -8,6 +9,9 @@ from medcat.components.base import BaseComponent, ComponentContract
 
 
 logger = Logger(__name__)
+
+
+_SENTINEL = object()
 
 
 class ContractViolation(Exception):
@@ -72,7 +76,6 @@ class WrappedMember:
                         spy.feedback.append((str(old), str(value)))
                     super().__setattr__(name, value)
 
-
         SpySubclass.__name__ = f"Spy({original_cls.__name__})"
         SpySubclass.__qualname__ = SpySubclass.__name__
         self.part.__class__ = SpySubclass
@@ -85,7 +88,82 @@ class WrappedMember:
 
 
 @contextmanager
+def spy_token_class(
+    token_cls: Type,
+    watched_attr: str,
+    access_type: AccessType,
+):
+    prev_getattr = token_cls.__dict__.get('__getattribute__', _SENTINEL)
+    prev_setattr = token_cls.__dict__.get('__setattr__', _SENTINEL)
+
+    per_instance_spied: dict[Any, list[Any]] = defaultdict(list)
+
+    def __getattribute__(self, name: str) -> Any:
+        val = object.__getattribute__(self, name)
+        if name == watched_attr:
+            per_instance_spied[self].append(str(val))
+        return val
+
+    def __setattr__(self, name: str, value: Any):
+        old = object.__getattribute__(self, name)
+        object.__setattr__(self, name, value)
+        if name == watched_attr:
+            per_instance_spied[self].append((str(old), str(value)))
+
+    if access_type == AccessType.READ:
+        token_cls.__getattribute__ = __getattribute__
+    elif access_type == AccessType.WRITE:
+        token_cls.__setattr__ = __setattr__
+    else:
+        raise ValueError(f"Unknown access type: {access_type}")
+    try:
+        yield per_instance_spied
+    finally:
+        if prev_getattr is _SENTINEL:
+            del token_cls.__getattribute__
+        else:
+            token_cls.__getattribute__ = prev_getattr
+
+        if prev_setattr is _SENTINEL:
+            token_cls.__setattr__ = object.__setattr__
+        else:
+            token_cls.__setattr__ = prev_setattr
+
+
+@contextmanager
 def wrap_relevant_parts(
+    doc: MutableDocument,
+    path: str,
+    access_type: AccessType = AccessType.READ,
+):
+    if path.startswith("doc."):
+        with wrap_relevant_persistant_parts(
+            doc, path, access_type
+        ) as feedbacks:
+            yield feedbacks
+    elif path.startswith("token."):
+        with wrap_relevant_token_cls(
+            doc, path, access_type
+        ) as feedbacks:
+            yield feedbacks
+
+
+@contextmanager
+def wrap_relevant_token_cls(
+    doc: MutableDocument,
+    path: str,
+    access_type: AccessType = AccessType.READ,
+):
+    _, attr_name = path.split(".", 1)
+    tkn_cls = type(next(iter(doc)))
+    with spy_token_class(
+        tkn_cls, attr_name, access_type
+    ) as per_instance_spied:
+        yield list(per_instance_spied.values())
+
+
+@contextmanager
+def wrap_relevant_persistant_parts(
     doc: MutableDocument,
     path: str,
     access_type: AccessType = AccessType.READ,
